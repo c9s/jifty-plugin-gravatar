78cae9e4703ff937811c5a66d6d0f5001036577e
Cornelius <cornelius.howl+git@gmail.com>
2009-03-14 22:04:30 +0800

    use Cache::File, and provide more options for caching

--- lib/Jifty/Plugin/Gravatar/View.pm
+++ lib/Jifty/Plugin/Gravatar/View.pm
@@ -1,22 +1,25 @@
 package Jifty::Plugin::Gravatar::View;
 use warnings;
 use strict;
+use Cache::File;
+use LWP::Simple qw();
+use Gravatar::URL;
 use Jifty::View::Declare -base;
-use Jifty::View::Declare::Helpers;
 
 template '/gravatar' => sub {
     my ( $self, $email ) = @_;
-    use Gravatar::URL;
     my $gravatar_url = gravatar_url( email => $email );
     my $gravatar_id = gravatar_id($email);
 
     div { { class is 'gravatar-image-wrapper' };
         # check config 
         my $config = Jifty->find_plugin('Jifty::Plugin::Gravatar');
-        if( $config->{Cache} ) {
+        if( $config->{LocalCache} ) {
+            Jifty->log->debug("[Gravatar] LocalCache: /=/gravatar/$gravatar_id" );
             img { { id is 'g-i-' . $gravatar_id, class is 'gravatar-image', src is '/=/gravatar/' . $gravatar_id }; };
         }
         else {
+            Jifty->log->debug("[Gravatar] Gravatar.com: $gravatar_url" );
             div { { class is 'gravatar-image-wrapper' };
                 img { { id is 'g-i-' . $gravatar_id, class is 'gravatar-image', src is $gravatar_url }; };
             };
@@ -28,13 +31,24 @@ template '/gravatar' => sub {
 template '/=/gravatar/image' => sub {
     my $config = Jifty->find_plugin('Jifty::Plugin::Gravatar');
     Jifty->handler->apache->content_type("image/jpeg");
-    Jifty->handler->apache->header_out(Expires =>  HTTP::Date::time2str(time() + ( $config->{Cache} || 3600 ) )); 
-    # XXX: use cache::file
+    Jifty->handler->apache->header_out(
+        Expires => HTTP::Date::time2str( time() + ( $config->{CacheExpire} * 60 || 3600 ))
+    );
     my $id = get('id');
     my $gravatar_url = gravatar_url( id => $id );
-    use LWP::Simple qw();
-    my $binary_image = LWP::Simple::get( $gravatar_url );
-    outs_raw( $binary_image );
+
+    my $cache_root = $config->{CacheRoot} || '/tmp/gravatar';
+
+    my $cache = Cache::File->new( cache_root => $cache_root );
+    my $image = $cache->get($id);
+    unless ($image) {
+        Jifty->log->debug('[Gravatar] Download Icon from: ' . $gravatar_url );
+        my $image = LWP::Simple::get($gravatar_url);
+        my $cache_expire = $config->{CacheFileExpire} || 3;
+        $cache->set( $id , $image , $cache_expire . ' minutes' );
+        Jifty->log->debug( '[Gravatar] Icon Cached: ' . $id  . " ($cache_expire minutes)" );
+    }
+    outs_raw( $image );
 };
 
 1;
